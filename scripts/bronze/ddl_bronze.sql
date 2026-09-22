02c91d0e5b159de1049565aa426b16c3f54ad998
Nandkishore Geete <96770989+devnkg@users.noreply.github.com>
2026-09-22 16:55:01 +0530

    Rename column cst_material_status to cst_marital_status

--- scripts/bronze/ddl_bronze.sql
+++ scripts/bronze/ddl_bronze.sql
@@ -23,7 +23,7 @@ CREATE OR REPLACE TABLE BRONZE.CRM_CUST_INFO (
     cst_key             VARCHAR(50),
     cst_firstname       VARCHAR(50),
     cst_lastname        VARCHAR(50),
-    cst_material_status VARCHAR(50),
+    cst_marital_status VARCHAR(50),
     cst_gndr            VARCHAR(50),
     cst_create_date     DATE
 );
